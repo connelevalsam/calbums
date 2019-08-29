c35eea8476bd2a134baff8c1d2a344d9f932219d
ConnelBLAZE <connelblaze@gmail.com>
2019-08-29 14:44:43 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import AlbumList from './src/components/AlbumList'
+
+const App = () => {
+  return (
+    <AlbumList />
+  );
+};
+
+export default App;
